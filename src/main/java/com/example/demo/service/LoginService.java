590b34a9d8c89dbf7b5b93cab990ff924c202e2d
yanghuining <542716590@qq.com>
2021-07-15 20:09:12 +0800

    登录

--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -1,8 +1,54 @@
+
 package com.example.demo.service;
+
 
 import com.example.demo.Result;
 import com.example.demo.entity.Login;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.LoginMapper;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-public interface LoginService {
-    public Result login(Login login);
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class LoginServiceImpl  {
+    @Autowired
+    private LoginMapper loginMapper;
+    public int mima(Login login) {
+     /*  if (StringUtils.isEmpty(login.getLoginName())){
+            return new Result(400,"账号不能为空","");
+        }
+        if (StringUtils.isEmpty(login.getPassword())){
+            return new Result(400,"密码不能为空","");
+        }
+*/
+        login.setPassword()=loginMapper.mima(login);
+
+
+        //通过登录名查询用户
+       // QueryWrapper<User> wrapper = new QueryWrapper();
+      //  wrapper.eq("login_name", login.getLoginName());
+       // User uer=userMapper.selectOne(wrapper);
+         //比较密码
+       // if (uer!=null&&uer.getPassword().equals(login.getPassword()))
+        //      return new Result(200,"",uer);
+        //}
+        /*//比较密码
+        if (uer!=null&&uer.getPassword().equals(login.getPassword())){
+            LoginVO loginVO=new LoginVO();
+            loginVO.setId(uer.getId());
+            //这里token直接用一个uuid
+            //使用jwt的情况下，会生成一个jwt token,jwt token里会包含用户的信息
+            loginVO.setToken(UUID.randomUUID().toString());
+            loginVO.setUser(uer);
+            return new Result(200,"",loginVO);
+        }
+
+         */
+        return new Result(401,"登录失败","");
+    }
 }
